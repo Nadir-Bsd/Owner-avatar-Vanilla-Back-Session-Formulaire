83f6f787d5a6788f63be146dc2425a94d266abdf
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-12-05 14:20:43 +0100

    END

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+// exo 1
+
+// if (isset($_GET["lastname"], $_GET["firstname"])) {
+//     $firstName = $_GET["firstname"];
+//     $lastName = $_GET["lastname"];
+
+//     var_dump($firstName, $lastName);
+// };
+
+// exo 2
+
+// if (isset($_GET["lastname"], $_GET["firstname"], $_GET["age"])) {
+//     $firstName = $_GET["firstname"];
+//     $lastName = $_GET["lastname"];
+//     $age = $_GET["age"];
+
+//     var_dump($firstName, $lastName, $age);
+// }else{
+//     echo "<p> il manque un ou des champs </p>";
+// };
+
+// exo 3
+
+// if (isset($_GET["startDate"], $_GET["endDate"])) {
+//     $startDate = $_GET["startDate"];
+//     $endDate = $_GET["endDate"];
+
+//     var_dump($endDate, $startDate);
+// }else{
+//     echo "<p> il manque un ou des champs </p>";
+// };
+
+// exo 4
+
+// if (isset($_GET["server"], $_GET["language"])) {
+//     $server = $_GET["server"];
+//     $language = $_GET["language"];
+
+//     var_dump($language, $server);
+// }else{
+//     echo "<p> il manque un ou des champs </p>";
+// };
+
+// exo 5
+
+// if (isset($_GET["week"])) {
+//     $week = $_GET["week"];
+
+//     var_dump($week);
+// }else{
+//     echo "<p> il manque un ou des champs </p>";
+// };
+
+// exo 6
+
+// if (isset($_GET["building"], $_GET["room"])) {
+//     $building = $_GET["building"];
+//     $room = $_GET["room"];
+
+//     var_dump($room, $building);
+// }else{
+//     echo "<p> il manque un ou des champs </p>";
+// };
