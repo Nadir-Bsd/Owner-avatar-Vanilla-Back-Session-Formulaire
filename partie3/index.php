6d595c10b8cd4a1a666d5f971ec7d6fee29937b4
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-12-05 21:28:59 +0100

    END: partie3

---
+++ partie3/index.php
@@ -0,0 +1,41 @@
+<?php
+// session_start();
+// header('Location: http://localhost:3000/partie3/second.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<!-- exo 3 -->
+    <form action="./second.php" method="post">
+        <label for="login">login</label><input type="text" name="login">
+        <label for="password">password</label><input type="password" name="password">
+        <input type="submit" value="send">
+    </form>
+
+    <?php
+    // exo 1
+    // var_dump($_SERVER["HTTP_USER_AGENT"]);
+    // var_dump($_SERVER['REMOTE_ADDR']);
+    // var_dump($_SERVER['SERVER_NAME']);
+
+    // exo 2
+    // setup 
+    // $_SESSION["firstName"] = "nadir";
+    // $_SESSION["lastName"] = "bensadi";
+    // $_SESSION["age"] = "23";
+
+    // return;
+
+    
+    ?>
+</body>
+
+</html>
---
+++ partie3/second.php
@@ -0,0 +1,24 @@
+<?php 
+// exo 2
+// session_start();
+
+// var_dump($_SESSION);
+
+// exo 3
+$cookie_name = "login";
+$cookie_value = $_POST["login"];
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "./second.php"); // 86400 = 1 day
+$cookie_name = "password";
+$cookie_value = $_POST["password"];
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "./second.php");
+
+
+// exo 4
+var_dump($_COOKIE);
+
+// exo 5
+$cookie_name = "login";
+$cookie_value = $_POST["login"];
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "./second.php");
+
+?>
